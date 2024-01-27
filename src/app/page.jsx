ac9d86ac8a236154eca4b95987b6fb375bd75d62
Sridhar <sridhar.fullstack.dev.india@gmail.com>
2024-01-27 19:04:28 +0530

    Initial commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 5000);
+    }, 50000);
 
     return () => clearTimeout(timer);
   }, []);
